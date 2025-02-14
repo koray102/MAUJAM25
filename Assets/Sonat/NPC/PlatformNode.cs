3501e3fc0b93b288c75b950cac2eefe704296050
cioccolata1113 <tansonat12@gmail.com>
2025-02-14 23:47:59 +0300

    NPC-1 Pişiyor

---
+++ Assets/Sonat/NPC/PlatformNode.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PlatformNode : MonoBehaviour
+{
+    // Bu node’dan atlanabilecek diðer platform node’larýný elle ayarlayýn.
+    public List<PlatformNode> neighbors;
+}
